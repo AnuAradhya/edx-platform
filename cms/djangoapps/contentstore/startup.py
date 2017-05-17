67164a3e9d7a602dff0bca8116c522a0b687c197
Gregory Martin <greg@edx.org>
2017-05-17 10:47:42 -0400

    test

--- cms/djangoapps/contentstore/startup.py
+++ cms/djangoapps/contentstore/startup.py
@@ -1,2 +1,2 @@
-""" will register signal handlers, moved out of __init__.py to ensure correct loading order post Django 1.7 """
-from signals import handlers  # pylint: disable=unused-import
+# """ will register signal handlers, moved out of __init__.py to ensure correct loading order post Django 1.7 """
+# from signals import handlers  # pylint: disable=unused-import
